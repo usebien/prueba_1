0f7a68953df67f4c993c5c8d194671c9a079d54d
usebien <112871417+usebien@users.noreply.github.com>
2023-12-25 19:36:53 -0300

    Update prueba_1.py

--- prueba_1.py
+++ prueba_1.py
@@ -2,18 +2,9 @@
 
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-st.set_page_config(page_title="prueba_1")
+info = st.container()
 
-dataset = st.container()
-dim = st.container()
-
-with dataset:
-    data = pd.read_csv('/Users/javi/Documents/APP/prueba_1/data/customers.csv',delimiter=",", decimal=".")
-    st.write(data.head(20))     
-
-with dim:
-    st.markdown('#### Dimensión de la información')
-    code = '''data.shape'''
-    st.code(code, language='python')
-    st.write(data.shape)
+with info:
+    st.markdown('**Sale con fritas**')
